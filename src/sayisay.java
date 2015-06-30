0d8714cd5882d1aff354c74b22c31a26e8096797
saltuksrc <saltuksaid_src@hotmail.com>
2015-06-30 16:39:08 +0300

    ekrana sayi yazdirma

---
+++ src/sayisay.java
@@ -0,0 +1,18 @@
+
+public class sayisay {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				
+		for	(int i=0;i<100;i++){
+			
+			System.out.println(i);
+			
+			
+		}
+
+	}
+
+}
