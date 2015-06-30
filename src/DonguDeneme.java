9dec3e8c1b64a90a4e2f0fd911a4a8bded50ea5f
saltuksrc <saltuksaid_src@hotmail.com>
2015-06-30 17:15:05 +0300

    döngü deneme

---
+++ src/DonguDeneme.java
@@ -0,0 +1,18 @@
+
+public class DonguDeneme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i=0 ; i<2; ++i){
+			
+			for (int j = 0; j<10; ++j){
+				System.out.println("j = "+ j +"   -    i = "+ i);
+			}
+			
+		}
+
+	}
+
+}
