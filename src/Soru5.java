062352fd0500a0be50a5ad7e892c25452232a5d7
Hasari Tosun <hasari@gmail.com>
2015-07-02 01:31:02 +0300

    hata duzeltildi

--- src/Soru5.java
+++ src/Soru5.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
-
 public class Soru5 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// Ekrandan okunan 3 karakter verinin (String) en büyüðünü bularak ekrana yazdýran bilgisayar programýný
-		//Java programlama dilinde yazýnýz. 
+		// Ekrandan okunan 3 karakter verinin (String) en bï¿½yï¿½ï¿½ï¿½nï¿½ bularak
+		// ekrana yazdï¿½ran bilgisayar programï¿½nï¿½
+		// Java programlama dilinde yazï¿½nï¿½z.
 
-		String[] yazi= new String[3];
+		String[] yazi = new String[3];
 		Scanner scan = new Scanner(System.in);
 		System.out.println("3 adet veri giriniz");
-		
-		for(int i = 0; i<yazi.length ; i++){
-			
-			System.out.print(i+1+".veri= ");
+
+		for (int i = 0; i < yazi.length; i++) {
+
+			System.out.print(i + 1 + ".veri= ");
 			yazi[i] = scan.next();
-			
+
 		}
-		for(int j=0 ; j<yazi.length ; j++){
-			
+		int index = 0;
+		for (int j = 1; j < yazi.length; j++) {
+			if (yazi[index].compareTo(yazi[j]) < 0) {
+				index = j;
+			}
 		}
-		
-		
-		
-		
-		
+		System.out.println("index=" + index + " yazi=" + yazi[index]);
+
 	}
 
 }
