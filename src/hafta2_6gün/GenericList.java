aa14b7470136dccdf781c598da07efca3bfb3afb
saltuksrc <saltuksaid_src@hotmail.com>
2015-07-09 23:30:24 +0300

    arraylist baslangıc

---
+++ src/hafta2_6gün/GenericList.java
@@ -0,0 +1,15 @@
+package hafta2_6gün;
+
+public class GenericList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		List<int> sayiListesi = new ArrayList<int>();
+		
+		
+	}
+
+}
---
+++ src/hafta2_6gün/Ogrenci.java
@@ -0,0 +1,9 @@
+package hafta2_6gün;
+
+public class Ogrenci {
+
+	
+ public String ad;
+ public int yas;
+	
+}
